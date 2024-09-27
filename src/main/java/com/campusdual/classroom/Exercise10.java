e10eb6c695d962cb8f1ea340c49a9087c47f8e91
Alberto Salvado Fernández <95777814+AlberSalva@users.noreply.github.com>
2024-09-27 13:40:32 +0200

    Implemented getBall(), used do-while in main to print ballColor and condition fulfillment

--- src/main/java/com/campusdual/classroom/Exercise10.java
+++ src/main/java/com/campusdual/classroom/Exercise10.java
@@ -4,22 +4,29 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Exercise10 {
 
-    //TODO ↓
-    // Imprimir el color de las pelotas que salgan, hasta que salgan 2 de color azul
-    // El mensaje a imprimir es → La bola es de color: COLOR_BOLA
-    // COLOR_BOLA puede ser → rojo, azul, verde
     public static void main(String[] args) {
-
+        int goal = 0;
+        String ballColor;
+        do {
+            ballColor = getBall();
+            System.out.println("La bola es de color: "+ ballColor);
+            if(ballColor.equals("azul")) {
+                goal++;
+            }
+        } while(goal < 2);
     }
 
-    //TODO ↓
-    // Un método que devuelva una cadena de texto con el color de una pelota (en minúscula). Dicho color se escogerá de
-    // manera aleatoria. Se empleará el método randomWithRange(int min, int max) para escoger un número aleatorio, que
-    // lo asociaremos al valor de un color de la siguiente manera:
-    // 1 → rojo
-    // 2 → azul
-    // 3 → verde
     public static String getBall() {
+        int colorValue = randomWithRange(1,4);
+        switch (colorValue) {
+            case 1:
+                return "verde";
+            case 2:
+                return "azul";
+            case 3:
+                return "rojo";
+        }
+
         return null;
     }
 
